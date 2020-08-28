2be47d07034b79c54c9212404923208673bd795a
ant-pearl <carlosalvatici@gmail.com>
2020-08-28 12:22:44 -0500

    V4

---
+++ products.js
@@ -0,0 +1,28 @@
+if (document.readyState == 'loading') {
+  document.addEventListener('DOMContentLoaded', ready)
+} else {
+  ready()
+}
+
+function ready() {
+  var products = document.getElementsByClassName('product-header')
+  for (var i = 0; i < products.length; i++) {
+    var product = products[i]
+    product.addEventListener('click', expandProduct)
+  }
+}
+
+function expandProduct(event) {
+  var product = event.target
+  console.log(product.parentElement.getElementsByClassName('product-summary'))
+  var productSummary = product.parentElement.getElementsByClassName('product-summary')[0]
+  if (productSummary.style.position == 'relative') {
+    console.log('yo')
+    productSummary.style.position = 'absolute'
+    productSummary.style.visibility = 'hidden'
+  } else {
+    console.log('nigga')
+    productSummary.style.position = 'relative'
+    productSummary.style.visibility = 'visible'
+  }
+}
